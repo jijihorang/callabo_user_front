1f524ec80dbfe3230ae22db75f2443ba9715290e
jijihorang <jiho7213@gmail.com>
2024-12-05 21:28:50 +0900

    choi CJH 사용자 정보 수정 및 추가 (2024-12-05)

--- src/apis/customer/customerAPI.ts
+++ src/apis/customer/customerAPI.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { UpdateCustomerDTO} from "../../types/customer/icustomer.ts";
 
 const host = 'http://localhost:8080/api2/customer';
 
@@ -6,4 +7,17 @@ const host = 'http://localhost:8080/api2/customer';
 export const toggleFollow = async (customerId: string, creatorId: string): Promise<void> => {
    const res =  await axios.post(`${host}/follow`, { customerId, creatorId });
    return res.data;
+};
+
+
+export const updateCustomer = async (
+    customerId: string,
+    customerData: UpdateCustomerDTO ): Promise<void> => {
+   try {
+      const res = await axios.put(`${host}/${customerId}`, customerData);
+      return res.data;
+   } catch (error) {
+      console.error("사용자 정보 업데이트 중 오류 발생:", error);
+      throw error;
+   }
 };
